ab625e4cd8e47ce5933b3bdba16617315666a721
Trojan0101 <aloysius.mon.05@gmail.com>
2024-12-20 00:39:10 +0530

    added service layer. Have to implement it.

---
+++ analyze_api/service/analyze_pdf_service.py
@@ -0,0 +1,13 @@
+"""
+###FILE DETAILS###
+File name: analyze_pdf_service.py
+Purpose: Method for analyze_pdf api end point
+
+###LICENSE DETAILS###
+License: MIT License
+Copyright (c) 2024 [Aloysius]
+"""
+
+
+def analyze_pdf_service():
+    pass
